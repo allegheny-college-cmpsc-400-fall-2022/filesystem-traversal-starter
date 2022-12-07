46268fc803c48b6c383ec27be84654b018e3f28d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-12-07 14:09:42 -0500

    chore: Add the main.py file with TODO markers about the implementation.

---
+++ tree/tree/main.py
@@ -0,0 +1,8 @@
+# TODO: implement a program that can:
+# --> perform a recursive traversal of a file system and collect all of the
+# files and directories nested within a specified directory
+# --> size all of the individual files (in a suitable size unit of measurement)
+# and display those sizes in the file tree displayed in the console
+#
+# TODO: Refer to the README.md file for more information about the program's
+# command-line arguments and the output that it is supposed to produce.
